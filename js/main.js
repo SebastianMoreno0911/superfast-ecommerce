a61a3ad798cd864792634949ffee99adcd4ed280
Sebastian Moreno <sebasmoreno744@gmail.com>
2025-06-18 12:14:15 -0500

    Add initial HTML structure and product data for ecommerce site
    - Created index.html with basic HTML structure and linked CSS and JS files.
    - Added main.js file containing an array of product objects for motorcycles and cars, including details such as id, name, image, category, and price.

---
+++ js/main.js
@@ -0,0 +1,105 @@
+//Productos
+const productos = [
+  //motos
+  {
+    id: "moto-1",
+    nombre: "BMW S1000RR",
+    imagen: "/img/motos/bmw s1000rr.png",
+    categoria: {
+      nombre: "Motos",
+      id: "motos",
+    },
+    precio: 22000,
+  },
+  {
+    id: "moto-2",
+    nombre: "GSX-R1000 GT",
+    imagen: "/img/motos/GSX S1000GT.png",
+    categoria: {
+      nombre: "Motos",
+      id: "motos",
+    },
+    precio: 14500,
+  },
+  {
+    id: "moto-3",
+    nombre: "Yamaha MT-09",
+    imagen: "/img/motos/mt09.png",
+    categoria: {
+      nombre: "Motos",
+      id: "motos",
+    },
+    precio: 10500,
+  },
+  {
+    id: "moto-4",
+    nombre: "Yamaha MT-10",
+    imagen: "/img/motos/mt10.png",
+    categoria: {
+      nombre: "Motos",
+      id: "motos",
+    },
+    precio: 15500,
+  },
+  {
+    id: "moto-5",
+    nombre: "Yamaha R1",
+    imagen: "/img/motos/r1.png",
+    categoria: {
+      nombre: "Motos",
+      id: "motos",
+    },
+    precio: 18500,
+  },
+  {
+    id: "moto-6",
+    nombre: "Kawasaki Z900",
+    imagen: "/img/motos/z900.png",
+    categoria: {
+      nombre: "Motos",
+      id: "motos",
+    },
+    precio: 9500,
+  },
+  {
+    id: "moto-7",
+    nombre: "Yamaha Tracer 9 GT+",
+    imagen: "/img/motos/tracer.png",
+    categoria: {
+      nombre: "Motos",
+      id: "motos",
+    },
+    precio: 15500,
+  },
+  //carros
+  {
+    id: "carro-1",
+    nombre: "Honda NSX (2ª gen)",
+    imagen: "/img/carros/Honda NSX (2ª gen).png",
+    categoria: {
+      nombre: "Carros",
+      id: "carros",
+    },
+    precio: 175000,
+  },
+  {
+    id: "carro-2",
+    nombre: "Nissan GT-R R35",
+    imagen: "/img/carros/Nissan GT-R R35.png",
+    categoria: {
+      nombre: "Carros",
+      id: "carros",
+    },
+    precio: 220000,
+  },
+  {
+    id: "carro-3",
+    nombre: "Toyota GR Supra GT4 EVO",
+    imagen: "/img/carros/TOYOTA GR SUPRA GT4 EVO.png",
+    categoria: {
+      nombre: "Carros",
+      id: "carros",
+    },
+    precio: 250000,
+  },
+];
